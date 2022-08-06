bdc7647c210905a172d405501cc90f6b2c9c97e3
Theo Browne <me@t3.gg>
2022-08-06 15:26:22 -0700

    hooks and such

--- lib/chat.dart
+++ lib/chat.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:tmi/tmi.dart' as tmi;
 
+import 'package:flutter_hooks/flutter_hooks.dart';
+
 // extension HexColor on Color {
 //   /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
 //   static Color fromHex(String hexString) {
@@ -18,6 +20,28 @@ import 'package:tmi/tmi.dart' as tmi;
 //       '${green.toRadixString(16).padLeft(2, '0')}'
 //       '${blue.toRadixString(16).padLeft(2, '0')}';
 // }
+
+class ChatPage extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final channelName = useState("");
+    // This widget is the home page of your application. It is stateful, meaning
+    // that it has a State object (defined below) that contains fields that affect
+    // how it looks.
+
+    // This class is the configuration for the state. It holds the values (in this
+    // case the title) provided by the parent (in this case the App widget) and
+    // used by the build method of the State. Fields in a Widget subclass are
+    // always marked "final".
+
+    if (channelName.value == "") return Text("hi");
+
+    return ChatView();
+  }
+
+  @override
+  State<ChatView> createState() => _ChatState();
+}
 
 class ChatView extends StatefulWidget {
   // This widget is the home page of your application. It is stateful, meaning
